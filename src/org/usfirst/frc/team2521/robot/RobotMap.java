7e86430482e9683619c859cc71b1790c7ee1e58d
Aubrey Anderson <andersonaubreye@gmail.com>
2015-01-08 17:09:18 -0800

    Changed package names and main class name to defaults

---
+++ src/org/usfirst/frc/team2521/robot/RobotMap.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team2521.robot;
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+    // For example to map the left and right motors, you could define the
+    // following variables to use with your drivetrain subsystem.
+    // public static int leftMotor = 1;
+    // public static int rightMotor = 2;
+    
+    // If you are using multiple modules, make sure to define both the port
+    // number and the module. For example you with a rangefinder:
+    // public static int rangefinderPort = 1;
+    // public static int rangefinderModule = 1;
+	
+	public static final int FRONT_RIGHT_MOTOR = 44;
+	public static final int FRONT_LEFT_MOTOR = 42;
+	public static final int REAR_RIGHT_MOTOR = 41;
+	public static final int REAR_LEFT_MOTOR = 43;
+	public static final int TRANSLATE_PORT = 0;
+	public static final int ROTATE_PORT = 1;
+	
+}
