3cecf4f217ecd0d3b4d9162c92bbe572d7220af6
Aubrey Anderson <andersonaubreye@gmail.com>
2015-01-23 15:53:00 -0800

    Changed conveyor motor from CANJaguar to Talons

--- src/org/usfirst/frc/team2521/robot/subsystems/Conveyor.java
+++ src/org/usfirst/frc/team2521/robot/subsystems/Conveyor.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team2521.robot.subsystems;
 
 import org.usfirst.frc.team2521.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -12,17 +12,19 @@ public class Conveyor extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-	private CANJaguar conveyor;
+	private Talon conveyor;
+	private Talon conveyor2;
 	
 	public Conveyor() {
-		conveyor = new CANJaguar(RobotMap.CONVEYOR_MOTOR);
-		conveyor.setPercentMode();
-		conveyor.enableControl(); 
+		conveyor = new Talon(0);
+		conveyor2 = new Talon(1);
+		
 	}
 	
 	
 	public void moveConveyor(double speed) {
 		conveyor.set(speed);
+		conveyor2.set(speed);
 	}
 	
     public void initDefaultCommand() {
